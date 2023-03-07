0e5e629660a34f93ab3ffb2b823fd4fe5a5d1ab9
Phodi <5670253+Phodi@users.noreply.github.com>
2023-03-07 20:34:28 +0700

    add option to generate random data

--- scan_provider/src/main.cpp
+++ scan_provider/src/main.cpp
@@ -2,6 +2,8 @@
 #include <fstream>
 #include <sstream>
 #include <signal.h>
+#include <cmath>
+#include <random>
 
 #include "sl_lidar.h"
 #include "sl_lidar_driver.h"
@@ -74,14 +76,45 @@ void check_config_var(std::string name, std::string var, std::string default="UN
     }
 }
 
-void check_config_var(std::string name, int var, int default=-1){
+bool check_config_var(std::string name, int var, int default=-1, bool will_exit=true){
     if(var != default){
         std::cout << name << ": " << var << std::endl;
+        return true;
     } else {
         std::cout << name << " is undefined, exiting" << std::endl;
-        exit(1);
+        if(will_exit){
+            exit(1);
+        }
+        return false;
     }
 }
+
+void gen_random_scan_data(std::vector<lc::MeasurementPoint>* output_data_point){
+    std::random_device rd;
+    std::mt19937 gen(rd());
+    std::uniform_real_distribution<> dis(0,3);
+    float r = dis(gen);
+    float cur_angle = 0;
+    float distance = 6000;
+    float quality = 0;
+    output_data_point->clear();
+    while(cur_angle + r < 360){
+        cur_angle += r;
+        r = r = dis(gen);
+
+        lc::MeasurementPoint point;
+		point.start_flag = false;
+		point.angle = cur_angle;
+		point.distance = distance;
+		point.quality = quality;
+
+		point.x = point.distance * std::cosf((point.angle - 270.0f) * M_PI / 180.0f);
+		point.y = point.distance * std::sinf((point.angle - 270.0f) * M_PI / 180.0f);
+
+		output_data_point->push_back(point);
+    }
+}
+
 
 int main(int argc, const char* argv[]) {
 
@@ -103,14 +136,50 @@ int main(int argc, const char* argv[]) {
     auto serial_baudrate = reader.GetInteger("serial","baudrate",-1);
     auto base_delay = reader.GetInteger("retry","base_delay", -1);
     auto max_delay = reader.GetInteger("retry","max_delay", -1);
+    auto gen_random = reader.GetBoolean("debug","gen_random",false);
     std::cout << "config.ini loaded" << std::endl;
     check_config_var("server_endpoint", server_endpoint);
     check_config_var("serial_port", serial_port);
     check_config_var("serial_baudrate", serial_baudrate);
     check_config_var("base_delay", base_delay);
     check_config_var("max_delay", max_delay);
+    if(gen_random){
+        std::cout << "gen_random: true" << std::endl;
+    }
     std::cout << "all config loaded" << std::endl;
     std::cout << std::endl;
+
+    if(gen_random){
+        std::cout << "[DEBUG] Generating random data" << std::endl;
+        std::cout << "==========[" << "scan" << "]==========" << std::endl;
+
+        std::vector<lc::MeasurementPoint> scan_data;
+        std::string scan_csv;
+
+        while(true){
+        delay(150);
+        // send data here
+        gen_random_scan_data(&scan_data);
+        scan_csv = points_to_csv_string(&scan_data);
+        cpr::Response r = cpr::Post(cpr::Url{ server_endpoint },
+            cpr::Body{ scan_csv },
+            cpr::Header{ {"Content-Type", "text/csv"} });
+        std::cout << "[HTTP-" << r.status_code << "] ";
+        if (r.status_code == 0) {
+            std::cout << "cannot connect to server" << std::endl;
+            continue;
+        }
+        if (r.text == "{\"status\": \"OK\"}") {
+            std::cout << "valid response" << std::endl;
+            continue;
+        }
+        else {
+            std::cout << "invalid response" << std::endl;
+            continue;
+        }
+        std::cout << std::endl;
+        }
+    }
 
     std::cout << "==========[" << "lidar" << "]==========" << std::endl;
     // start connection
@@ -159,7 +228,7 @@ int main(int argc, const char* argv[]) {
         std::string scan_csv = points_to_csv_string(&scan_data);
 
         // send data here
-        cpr::Response r = cpr::Post(cpr::Url{ SERVER_ENDPOINT },
+        cpr::Response r = cpr::Post(cpr::Url{ server_endpoint },
             cpr::Body{ scan_csv },
             cpr::Header{ {"Content-Type", "text/csv"} });
         std::cout << "[HTTP-" << r.status_code << "] ";
